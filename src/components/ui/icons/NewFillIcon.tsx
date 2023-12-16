cf16d03c072d97d6b9780ce4438643a7662daa0a
Noma <wiostz98kr@gmail.com>
2023-12-16 14:01:55 +0900

    Feat: Implement icon components using react-icons

---
+++ src/components/ui/icons/NewFillIcon.tsx
@@ -0,0 +1,7 @@
+import { BsPlusSquareFill } from 'react-icons/bs';
+
+function NewFillIcon() {
+  return <BsPlusSquareFill className='w-7 h-7' />;
+}
+
+export default NewFillIcon;
---
+++ src/components/ui/icons/SearchFillIcon.tsx
@@ -0,0 +1,7 @@
+import { RiSearchFill } from 'react-icons/ri';
+
+function SearchIcon() {
+  return <RiSearchFill className='w-7 h-7' />;
+}
+
+export default SearchIcon;
